0999ec89b841b9dd2a02b6d14abd0ca21afdaa34
ethanjd23 <ethanjd23@gmail.com>
2021-02-24 10:45:24 -0600

    starting on class-based component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,10 +1,36 @@
-const App = () => {
+import React, { useState, Component } from 'react';
+import Weather from "./Weather";
 
-    return (
+// const App = (props) => {
+
+//     const [city, setCity] = useState("Birmingham");
+//     const [opinion, setOpinion] = useState("It's great.")
+
+//     return (
+//         <>
+//             <Weather sun="sunny" degrees="61" />
+//             <p>{opinion}</p>
+//             <p>You're in {city}.</p>
+//             <input type="text" placeholder="your city" onChange={e => setCity(e.target.value)}></input>
+//         </>
+//     )
+   
+// }
+
+class App extends Component {
+    render() {
+    const [city, setCity] = useState("Birmingham");
+    const [opinion, setOpinion] = useState("It's great.")
+    return(    
         <>
-            <h1>Hello World</h1>
-        </>
+             <Weather sun="sunny" degrees="61" />
+             <p>{opinion}</p>
+             <p>You're in {city}.</p>
+             <input type="text" placeholder="your city" onChange={e => setCity(e.target.value)}></input>
+         </>
     )
+    }
+
 }
 
 export default App;
---
+++ src/Components/Weather.jsx
@@ -0,0 +1,10 @@
+
+
+const Weather = (props) => {
+
+    return (
+        <h1>It's {props.sun} today and {props.degrees} degrees.</h1>
+)
+}
+
+export default Weather;
